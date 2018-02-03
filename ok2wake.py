fc5aba71cc39f95653bce64d45e42b3b0b4ff029
Hepler <dhepler@netapp.com>
2018-02-03 14:04:28 +0100

    Removed Old script, added the log file (this is the first test, so beta!)

--- ok2wake.py
+++ ok2wake.py
@@ -30,6 +30,7 @@ def loop():
   currentMinute =  datetime.datetime.now().minute
   day_date = datetime.date(currentYear, currentMonth, currentDay) 
   currentDay = day_date.isoweekday() 	#Monday is 1 and Sunday is 7
+  logFile = "/mnt/OK2Wake_logs/currentYear_currentMonth_currentDay.log"
 # Import the correct config file 
   if (currentDay >= 1 and currentDay <= 4):
    settings.read('weekday.ini')
@@ -50,13 +51,13 @@ def loop():
 
 # Looping - run according to the current time
   if os.path.exists(my_breakfile):
-   print 'break break break!'
+   echo 'break! day_date' >> logFile
    destroy()
    break
 
   elif (currentHour == hourToSleep-1):
-   print currentHour
-   print 'Almost time to go to sleep'
+   echo 'Almost time to go to sleep, its currentHour:currentMinute' >> logFile
+   print 'Almost time to go to sleep, its currentHour:currentMinute'
    countA = 0
    while countA < 10:
     GPIO.output(YELLOW_LED, GPIO.HIGH)  # led on
@@ -70,12 +71,13 @@ def loop():
 
   elif (currentHour >= hourToSleep or currentHour < hourOKtoWake-2):
    print currentHour
-   print 'Go to sleep!'
+   echo 'Go to sleep! its currentHour:currentMinute' >> logFile
    GPIO.output(RED_LED, GPIO.HIGH)   # led on
 #  Sleep before next loop (long)
    time.sleep(3600)
 
   elif (currentHour >= hourOKtoWake-2 and currentHour < hourOKtoWake):
+   echo 'Almost time to wake up, its currentHour:currentMinute' >> logFile
    print currentHour
    print 'almost time to wake'
    GPIO.output(RED_LED, GPIO.HIGH)   # led on
@@ -84,6 +86,7 @@ def loop():
    
   elif (currentHour >= hourOKtoWake and currentMinute >= minuteOKtoWake and currentHour < hourOKtoLeaveRoom):
    print currentHour
+   echo 'OK to wake up, but stay in room, its currentHour:currentMinute' >> logFile
    print 'OK to wake up, but stay in your room!'
    GPIO.output(RED_LED, GPIO.LOW)  # led off
    GPIO.output(YELLOW_LED, GPIO.HIGH)  # led on
@@ -91,6 +94,7 @@ def loop():
    time.sleep(60)
     
   elif (currentHour == hourOKtoLeaveRoom and currentMinute >= minuteOKtoLeaveRoom):
+   echo 'OK to leave room, its currentHour:currentMinute' >> logFile
    print currentHour
    print 'OK to leave room!'
    GPIO.output(RED_LED, GPIO.LOW)  # led off
@@ -99,6 +103,7 @@ def loop():
    time.sleep(3600)
 
   elif (currentHour >= hourToSleep):
+   echo 'Go to sleep, its currentHour:currentMinute' >> logFile
    print currentHour
    print 'Go to sleep!'
    GPIO.output(RED_LED, GPIO.HIGH)   # led on
@@ -106,6 +111,7 @@ def loop():
    time.sleep(3600)
 
   else:
+   echo 'Not active - currentHour:currentMinute' >> logFile
    print currentHour
    GPIO.output(RED_LED, GPIO.LOW)  # led off
    GPIO.output(YELLOW_LED, GPIO.LOW)  # led off
@@ -113,6 +119,7 @@ def loop():
    time.sleep(3600)
  
 def destroy():
+ echo 'Script Ending - currentHour:currentMinute' >> logFile
  GPIO.output(RED_LED, GPIO.LOW)      # RED led off
  GPIO.output(YELLOW_LED, GPIO.LOW)      # Yellow led off  
  GPIO.cleanup()         # Release resource
